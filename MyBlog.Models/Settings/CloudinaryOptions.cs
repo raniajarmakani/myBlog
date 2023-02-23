63c39b6dda48188b731d77f9c88ff49251a86cb4
raniajarmakani <rania_sj@hotmail.com>
2023-02-23 11:17:39 +0200

    Settings+Exception folders added to Models

---
+++ MyBlog.Models/Settings/CloudinaryOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.Models.Settings
+{
+    public class CloudinaryOptions
+    {
+        public string cloudName { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
---
+++ MyBlog.Services/IPhotoServices.cs
@@ -0,0 +1,15 @@
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace MyBlog.Services
+{
+    public interface IPhotoServices
+    {
+
+        public Task<ImageUploadResult> AddPhotoAsync(IFormFile photo);
+        public Task<DeletionResult> DeletePhotoAsync(string publicID);
+      
+    }
+}
